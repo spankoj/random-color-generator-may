feba4dd44c3572a1cc2e725a18d80d2ed5bcff17
Janos Spanyol <77097341+spankoj@users.noreply.github.com>
2021-05-07 12:20:43 +0200

    Add final code

--- index.js
+++ index.js
@@ -1,67 +1,69 @@
-// ------------------NPM packages--------------------------------------
+// NPM packages
+
 const randomColor = require('randomcolor');
 const chalk = require('chalk');
 const prompt = require('prompt');
 
-//------------------Command line arguments------------------------------
+// Command line arguments
+
 const luminosity = process.argv[2];
 const hue = process.argv[3];
 
-// ----------------Random color generator with luminosity and hue-----
+// Ask user input with Promt pkg
 
-// console.log(color);
-
-// ----------------Drawing the pattern----------------------------------
 if (luminosity === 'ask') {
   prompt.start();
   prompt.get(['luminosity', 'hue'], function (err, result) {
-    //
-    // Log the results.
-    //
     console.log('Command-line input received:');
     console.log('  luminosoty: ' + result.luminosity);
     console.log('  hue: ' + result.hue);
+
+    // Generating Random color with luminosity and hue options
 
     const color = randomColor({
       luminosity: result.luminosity,
       hue: result.hue,
     });
 
+    // Drawing the pattern with colord text:Chalk pkg
+
     for (i = 0; i < 3; i++) {
-      console.log(chalk.hex(color).bold('##########################')); // makes pound with the rendom color
+      console.log(chalk.hex(color).bold('##########################'));
     }
     for (i = 0; i < 1; i++) {
-      console.log(chalk.hex(color).bold(`###                    ###`)); // makes pound with the rendom color
+      console.log(chalk.hex(color).bold(`###                    ###`));
     }
     for (i = 0; i < 1; i++) {
-      console.log(chalk.hex(color).bold(`###      ${color}       ###`)); // makes pound with the rendom color
+      console.log(chalk.hex(color).bold(`###      ${color}       ###`));
     }
     for (i = 0; i < 1; i++) {
-      console.log(chalk.hex(color).bold(`###                    ###`)); // makes pound with the rendom color
+      console.log(chalk.hex(color).bold(`###                    ###`));
     }
     for (i = 0; i < 3; i++) {
-      console.log(chalk.hex(color).bold('##########################')); // makes pound with the rendom color
+      console.log(chalk.hex(color).bold('##########################'));
     }
   });
-} else {
+}
+// In case no input run CLI app with random color
+else {
   const colorGiven = randomColor({
     luminosity: luminosity,
     hue: hue,
   });
 
   for (i = 0; i < 3; i++) {
-    console.log(chalk.hex(colorGiven).bold('##########################')); // makes pound with the rendom colorGiven
+    console.log(chalk.hex(colorGiven).bold('##########################'));
   }
   for (i = 0; i < 1; i++) {
-    console.log(chalk.hex(colorGiven).bold(`###                    ###`)); // makes pound with the rendom colorGiven
+    console.log(chalk.hex(colorGiven).bold(`###                    ###`));
   }
   for (i = 0; i < 1; i++) {
-    console.log(chalk.hex(colorGiven).bold(`###      ${colorGiven}       ###`)); // makes pound with the rendom colorGiven
+    console.log(chalk.hex(colorGiven).bold(`###      ${colorGiven}       ###`));
   }
   for (i = 0; i < 1; i++) {
-    console.log(chalk.hex(colorGiven).bold(`###                    ###`)); // makes pound with the rendom colorGiven
+    console.log(chalk.hex(colorGiven).bold(`###                    ###`));
   }
   for (i = 0; i < 3; i++) {
-    console.log(chalk.hex(colorGiven).bold('##########################')); // makes pound with the rendom colorGiven
+    console.log(chalk.hex(colorGiven).bold('##########################'));
   }
 }
